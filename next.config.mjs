ef232cd5c69fbdbff49033a3abb528f58bbc1cff
Naman rai <namanrai309@gmail.com>
2024-11-27 19:43:40 +0530

    evolve-api

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/evolve",
+        destination: "http://13.235.78.233:3000",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
